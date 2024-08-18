1f9ce16ae43aa636327062e2e773c85abef6a18e
Priestly Bulan Lobo, S.Kom <62798472+Riess-01@users.noreply.github.com>
2024-08-18 12:58:41 +0800

    Add files via upload

---
+++ Python Training 24-1/Module/OperatorMatematika.py
@@ -0,0 +1,15 @@
+class OperatorMatematika :
+
+	
+	
+	def penjumlahan (a,b) :
+	    return a+b
+	    
+	def pengurangan (a,b) :
+	    return a-b
+	    
+	def perkalian (a,b) :
+	    return a*b
+	    
+	def pembagian (a,b) :
+	    return a/b
